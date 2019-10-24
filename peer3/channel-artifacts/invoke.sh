d2925ef401fa22bb7cc1f9c6bd7ea51765d6d448
Ubuntu <ubuntu@ip-172-26-9-218.ap-northeast-2.compute.internal>
2019-10-24 14:01:34 +0000

    Change peer3 scripts

--- peer3/channel-artifacts/invoke.sh
+++ peer3/channel-artifacts/invoke.sh
@@ -3,6 +3,6 @@ echo $FABRIC_PATH
 
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_MSPCONFIGPATH=$FABRIC_PATH/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
-export CORE_PEER_ADDRESS=peer3.org1.example.com:7051
+export CORE_PEER_ADDRESS=peer1.org1.example.com:7051
 
-peer chaincode invoke -o orderer0.example.com:7050 -C ch1 -n testnetCC -c '{"Args":["invoke","a","b","50"]}'
+peer chaincode invoke -o orderer0.example.com:7050 --tls true --cafile $FABRIC_PATH/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C ch1 -n testnetCC -c '{"Args":["invoke","a","b","50"]}'
