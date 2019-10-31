e397cda7bd78da5d07b7db7deb8791f9d6d735b1
anstjaos <vv_eros@naver.com>
2019-10-31 05:03:16 +0000

    TLS on

--- peer0/channel-artifacts/updateAnchor.sh
+++ peer0/channel-artifacts/updateAnchor.sh
@@ -4,4 +4,4 @@ echo $FABRIC_PATH
 export CORE_PEER_LOCALMSPID="Org0MSP"
 export CORE_PEER_MSPCONFIGPATH=$FABRIC_PATH/crypto/peerOrganizations/org0.example.com/users/Admin@org0.example.com/msp
 export CORE_PEER_ADDRESS=peer0.org0.example.com:7051
-peer channel update -o orderer0.example.com:7050 -c ch1 -f ./Org0MSPanchors.tx --tls true --cafile $FABRIC_PATH/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+peer channel update -o orderer0.example.com:7050 -c ch1 -f ./Org0MSPanchors.tx --tls --cafile $FABRIC_PATH/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
--- peer3/channel-artifacts/invoke.sh
+++ peer3/channel-artifacts/invoke.sh
@@ -5,4 +5,4 @@ export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_MSPCONFIGPATH=$FABRIC_PATH/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=peer1.org1.example.com:7051
 
-peer chaincode invoke -o orderer0.example.com:7050 --tls true --cafile $FABRIC_PATH/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C ch1 -n testnetCC -c '{"Args":["invoke","a","b","50"]}'
+peer chaincode invoke -o orderer0.example.com:7050 --tls --cafile $FABRIC_PATH/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C ch1 -n testnetCC -c '{"Args":["invoke","a","b","50"]}'
