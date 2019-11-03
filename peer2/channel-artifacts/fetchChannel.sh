f22a1c5cf7b85e911a0a6c86d17ee8a31f836392
anstjaos <vv_eros@naver.com>
2019-11-03 11:44:53 +0000

    Change sh

--- peer2/channel-artifacts/fetchChannel.sh
+++ peer2/channel-artifacts/fetchChannel.sh
@@ -5,5 +5,5 @@ CORE_PEER_LOCALMSPID="Org1MSP" \
 CORE_PEER_TLS_ROOTCERT_FILE=$FABRIC_PATH/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt \
 CORE_PEER_MSPCONFIGPATH=$FABRIC_PATH/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp \
 CORE_PEER_ADDRESS=peer0.org1.example.com:7051 \
-peer channel fetch 0 ch1.block -o orderer0.example.com:7050 -c ch1 --tls true --cafile $FABRIC_PATH/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+peer channel fetch 0 ch1.block -o orderer0.example.com:7050 -c ch1 --tls --cafile $FABRIC_PATH/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
